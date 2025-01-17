0cb75b586235b39c6f7764a228e9ed08d6b41942
Sidvi733 <sruthi.p@clientservertech.com>
2025-01-17 20:07:52 +0530

    Assgmnt

--- src/Assaignment.java
+++ src/Assaignment.java
@@ -2,5 +2,14 @@
 
 
 public class Assaignment {
-    
+//    public static void main(String[] args) {
+//        for(int i=5;i>=1;i--){
+//            System.out.println(i);
+//        }
+//    }
+      public static void main(String[] args) {
+           for(int i=1;i<=5;i++){
+                 System.out.println(i);
+           }
+      }
 }
