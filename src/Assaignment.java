05111cd3b5b123deaed6cf0038baa810a0a005b9
Sidvi733 <sruthi.p@clientservertech.com>
2025-01-17 20:56:46 +0530

    Assgmnts

--- src/Assaignment.java
+++ src/Assaignment.java
@@ -3,23 +3,42 @@
 
 public class Assaignment {
     public static void main(String[] args) {
-        for (int i = 5; i >= 1; i--) {
-            System.out.println(i);
+        int num=12345;
+        while (num>0){
+            int dig=num%10;
+            System.out.println(dig);
+            num=num/10;
         }
+//        for (int i = 5; i >= 1; i--) {
+//            System.out.println(i);
+//        }
+        //5
+        //4
+        //3
+        //2
+        //1
         System.out.println();
 
         for (int i = 1; i <= 5; i++) {
             System.out.println(i);
         }
+        //1
+        //2
+        //3
+        //4
+        //5
         System.out.println();
 
-        int num=1;
+        int num1=1;
         for(int i=1;i<=3;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(num+" ");
-                num++;
+                System.out.print(num1+" ");
+                num1++;
             }
             System.out.println();
+            //1
+            //2 3
+            //4 5 6
         }
     }
 }
